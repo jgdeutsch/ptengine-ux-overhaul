46e6f970bec570f9e08864acb8a81c392b9eb669
Jeff Deutsch <jgdeutsch@gmail.com>
2015-08-14 15:36:17 -0700

    James changed something

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <title>Ptengine - Web Analytics and Heat Map Tool</title>
     <!-- Meta -->
-    <!-- MORK -->
+    <!-- James Deutsch edited line 14 -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="">
