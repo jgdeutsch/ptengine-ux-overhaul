4d57dd4db362145cc84cfd359ba43a8b0d589e71
Jeff Deutsch <jgdeutsch@gmail.com>
2015-08-14 15:33:26 -0700

    pish

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <title>Ptengine - Web Analytics and Heat Map Tool</title>
     <!-- Meta -->
-    <!-- comment -->
+    <!-- MORK -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="">
